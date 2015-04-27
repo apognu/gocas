e93bd226e80c5ff197e4be5cdf7d452900f82ba5
Antoine POPINEAU <antoine.popineau@appscho.com>
2015-04-27 18:13:04 +0200

    Redirecting / and {prefix} to {prefix}/login.

--- gocas.go
+++ gocas.go
@@ -15,16 +15,24 @@ var (
 	config = flag.String("config", "/etc/gocas.yaml", "path to GoCAS configuration file")
 )
 
+func redirect(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Location", util.Url("/login"))
+	w.WriteHeader(http.StatusFound)
+}
+
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 
 	util.SetConfig(*config)
 
-	r := mux.NewRouter()
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/", redirect).Methods("GET")
+
 	prefix := util.GetConfig().UrlPrefix
 	sr := r
 	if prefix != "" {
 		sr = r.PathPrefix(prefix).Subrouter()
+		sr.HandleFunc("/", redirect)
 	}
 	sr.HandleFunc("/login", loginRequestorHandler).Methods("GET")
 	sr.HandleFunc("/login", loginAcceptorHandler).Methods("POST")
